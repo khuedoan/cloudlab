595aa609a8eccda2fe64268f5110ea1932a60c77
Khue Doan <mail@khuedoan.com>
2025-08-27 18:18:56 +0700

    chore(argocd): upgrade to v3.1.0 stable

--- infra/_modules/bootstrap/argocd.tf
+++ infra/_modules/bootstrap/argocd.tf
@@ -4,7 +4,7 @@ resource "helm_release" "argocd" {
   create_namespace = true
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "8.1.3"
+  version          = "8.3.0"
   timeout          = 60 * 10
   max_history      = 1 # Revert with Terraform instead
 
@@ -12,11 +12,6 @@ resource "helm_release" "argocd" {
     yamlencode({
       global = {
         domain = "argocd.${var.cluster_domain}"
-        image = {
-          # TODO override to use the latest development version for first class OCI support
-          # Remove once ArgoCD 3.1.0 stable is released
-          tag = "v3.1.0-rc1"
-        }
       }
       configs = {
         params = {
@@ -40,8 +35,7 @@ resource "helm_release" "argocd" {
 
         }
         rbac = {
-          # TODO remove k3d-specific logic?
-          "policy.default" = var.platform == "k3d" ? "role:admin" : "role:readonly"
+          "policy.default" = "role:readonly"
         }
       }
       server = {
@@ -51,12 +45,8 @@ resource "helm_release" "argocd" {
           annotations = {
             "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
           }
-          tls = !(var.platform == "k3d")
+          tls = true
         }
-      }
-      repoServer = {
-        hostNetwork = var.platform == "k3d"
-        dnsPolicy   = var.platform == "k3d" ? "ClusterFirstWithHostNet" : "ClusterFirst"
       }
       dex = {
         enabled = false
