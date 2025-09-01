ace48f0f62828b0546f9de672e58ced3d1898825
Khue Doan <mail@khuedoan.com>
2025-09-01 16:23:34 +0700

    feat(infra): install registry with Nix
    Maybe everything else can be done with Nix?

--- infra/_modules/nixos/profiles/k3s-addons.nix
+++ infra/_modules/nixos/profiles/k3s-addons.nix
@@ -15,6 +15,30 @@
               --export > $out/flux.yaml
           '';
         };
+        registry-namespace = {
+          content = {
+            apiVersion = "v1";
+            kind = "Namespace";
+            metadata = {
+              name = "registry";
+            };
+          };
+        };
+        registry = {
+          source = pkgs.runCommand "registry-install-manifest" {
+            nativeBuildInputs = [ pkgs.kubernetes-helm ];
+          } ''
+            mkdir -p $out
+            helm template --skip-tests registry ${
+                pkgs.fetchurl {
+                  url = "https://github.com/project-zot/helm-charts/releases/download/zot-0.1.67/zot-0.1.67.tgz";
+                  sha256 = "118js6m16fvzxxjznydjp6kip67548s6l47zvp0fjjsz9fzz438r";
+                }
+              } \
+              --namespace registry \
+              --values ${./values/registry.yaml} > $out/registry.yaml
+          '';
+        };
       };
     };
   };
