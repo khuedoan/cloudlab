bfe983ee6cf8f0961df0183ce79a6d5aa983cd85
Khue Doan <mail@khuedoan.com>
2025-09-04 22:45:31 +0700

    feat(infra): deploy in-cluster registry with airgap artifacts

--- infra/_modules/nixos/profiles/k3s-addons.nix
+++ infra/_modules/nixos/profiles/k3s-addons.nix
@@ -13,6 +13,54 @@
               --export > $out/flux.yaml
           '';
         };
+        registry = {
+          source = pkgs.runCommand "registry-install-manifest" {} ''
+            mkdir -p $out
+
+            ${pkgs.kubectl}/bin/kubectl create namespace registry \
+              --output=yaml \
+              --dry-run=client \
+              > $out/registry-namespace.yaml
+
+            ${pkgs.kubernetes-helm}/bin/helm template \
+              --skip-tests \
+              --namespace=registry \
+              registry \
+              ${pkgs.fetchurl {
+                url = "https://github.com/project-zot/helm-charts/releases/download/zot-0.1.80/zot-0.1.80.tgz";
+                sha256 = "093frrijhycb4dsdwy7h99j5klrphr4xa0chnibmkqiy5znlaf4w";
+              }} \
+              --version=0.1.67 \
+              --values=${pkgs.writeText "values.yaml" (builtins.toJSON {
+                nameOverride = "registry";
+                image = {
+                  repository = "docker.io/khuedoan/zot-airgap";
+                };
+                mountConfig = true;
+                configFiles = {
+                  "config.json" = (builtins.toJSON {
+                    storage = {
+                      rootDirectory = "/var/lib/registry";
+                      subpaths = {
+                        "/vendor" = {
+                          rootDirectory = "/var/lib/registry-airgap";
+                        };
+                      };
+                    };
+                    http = {
+                      address = "0.0.0.0";
+                      port = "5000";
+                    };
+                    log.level = "debug";
+                  });
+                };
+                service = {
+                  type = "ClusterIP";
+                };
+              })} \
+              > $out/registry.yaml
+          '';
+        };
         gitops = {
           content = [
             {
@@ -28,7 +76,7 @@
                 # url = "http://forgejo-http.forgejo.svc.cluster.local:3000/khuedoan/cloudlab";
                 url = "https://code.khuedoan.com/khuedoan/cloudlab";
                 ref = {
-                  branch = "master";
+                  branch = "fast-cluster"; # TODO back to master ofc
                 };
               };
             }
