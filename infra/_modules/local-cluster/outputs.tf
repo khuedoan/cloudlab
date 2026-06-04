b6c00bf7944e28b713312ef84d9e3517aabb4f03
Khue Doan <mail@khuedoan.com>
2026-06-04 21:06:28 +0700

    chore: remove local environment
    Likely switch to NixOS VM.

--- infra/_modules/local-cluster/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "name" {
-  value = var.name
-}
-
-output "credentials" {
-  value     = k3d_cluster.this.credentials[0]
-  sensitive = true
-}
--- infra/_modules/local-cluster/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    k3d = {
-      source  = "moio/k3d"
-      version = "0.0.12"
-    }
-  }
-}
